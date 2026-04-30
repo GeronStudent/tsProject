87a12d77c8bc814bb32a30235877efde37b20eed
Your Name <you@example.com>
2026-04-30 19:51:21 +0500

    удалила лишний код

--- src/scripts/geron.ts
+++ src/scripts/geron.ts
@@ -1,21 +1,4 @@
-// enum Answer {
-//     A = "A",
-//     B = "B",
-//     C = "C"
-// }
 
-// let answer: Answer = Answer.A as Answer
-
-// switch (answer) {
-//   case Answer.A:
-//   case Answer.B:
-//     console.log("2 score");
-//     case Answer.C:
-//         console.log("3 score");
-//         break;
-//   default:
-//     console.log("Неизвестный ответ");
-// }
 
 // Задача:
 
